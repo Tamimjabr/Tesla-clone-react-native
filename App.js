d814629f71106732bd471902b54102ba0044d614
Tamim Jabr <tj222kg@student.lnu.se>
2022-04-17 16:49:31 +0200

    first commit

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.carContainer}>
+        <View style={styles.titles}>
+          <Text style={styles.title}>Model S</Text>
+          <Text style={styles.subTitle}>Pris fr. 500 000kr</Text>
+        </View>
+      </View>
+      <StatusBar style='auto' />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -15,6 +20,23 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
-});
+  carContainer: {
+    width: '100%',
+    height: '100%'
+  },
+  titles: {
+    width: '100%',
+    marginTop: '30%',
+    alignItems: 'center'
+  },
+  title: {
+    fontSize: 25,
+    fontWeight: '600'
+  },
+  subTitle: {
+    fontSize: 16,
+    fontFamily: '#5c5e62'
+  }
+})
